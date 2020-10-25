181df85e267d2437fa4413280301cf85f4ae8713
Yolanda Collins <yolanda.collins@firemind.com>
2020-10-25 14:19:20 +0000

    images added to carousel with captions

--- src/js/constant/image-items.js
+++ src/js/constant/image-items.js
@@ -1,13 +1,64 @@
-import MainMenu from '../../images/menu.png'
-import GWLevel from '../../images/generate-wod-level.png'
+import MainMenu from '../../images/menu.jpg'
+import Settings from '../../images/settings.jpg'
+import EquipmentList from '../../images/equipment-list.jpg'
+import GWLevel from '../../images/generate-wod-level.jpg'
+import GWMuscle from '../../images/generate-wod-muscle.jpg'
+import GWType from '../../images/generate-wod-type.jpg'
+import GWEquipment from '../../images/generate-wod-equipment.jpg'
+import GWod from '../../images/workout-generated.jpg'
+import Timer from '../../images/timer.jpg'
+import ExerciseLibrary from '../../images/exercise-library.jpg'
+import Run from '../../images/track-my-run.jpg'
+import SavedWods from '../../images/saved-workouts.jpg'
+
 
 export const items = [
   {
     src: MainMenu,
-    altText: 'Slide 1',
+    altText: 'Main menu screen',
+  },
+  {
+    src: Settings,
+    altText: 'Settings screen'
+  },
+  {
+    src: EquipmentList,
+    altText: 'Equipment list screen'
   },
   {
     src: GWLevel,
-    altText: 'Slide 2'
+    altText: 'Generated workout select level screen'
+  },
+  {
+    src: GWMuscle,
+    altText: 'Generated workout select muscle group screen'
+  },
+  {
+    src: GWType,
+    altText: 'Generated workout select type screen'
+  },
+  {
+    src: GWEquipment,
+    altText: 'Generated workout select Equipment list screen'
+  },
+  {
+    src: GWod,
+    altText: 'Generated workout screen'
+  },
+  {
+    src: Timer,
+    altText: 'Timer screen'
+  },
+  {
+    src: ExerciseLibrary,
+    altText: 'Exercise Library screen'
+  },
+  {
+    src: Run,
+    altText: 'Track my run screen'
+  },
+  {
+    src: SavedWods,
+    altText: 'Saved workouts screen'
   }
 ]
